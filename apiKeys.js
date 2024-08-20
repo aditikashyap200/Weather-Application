f2afac5abb03f455fe5d9dfa78c7bec05dca10fc
aditikashyap200 <151127817+aditikashyap200@users.noreply.github.com>
2024-08-21 01:21:35 +0530

    Add files via upload

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "17d30c767cf12af54eeceead535a3b1f",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
